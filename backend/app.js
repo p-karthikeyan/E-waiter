3c37277b06a09d0c5830ba1251cb644b1e5d3a04
p-karthikeyan <keyan4676@gmail.com>
2023-08-26 22:33:56 +0530

    restaurant/register api written

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,19 @@
 const express = require('express')
 const cors = require('cors')
+const bcrypt=require('bcrypt')
+const csvParser = require('csv-parser');
+const fs = require('fs');
 
 const dotenv=require('dotenv')
 dotenv.config()
 
-const mongoose = require('mongoose')
-const con_uri=process.env.DATABASE_URI
-
 app=express()
-
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(cors())
 
-const Restaurant=require('./models/restaurantmodel')
-
+const mongoose = require('mongoose')
+const con_uri=process.env.DATABASE_URI
 mongoose.connect(con_uri,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -22,6 +21,36 @@ mongoose.connect(con_uri,{
     app.listen(process.env.PORT,()=>console.log(`listening to the port 5000...`))
 }).catch(err=>console.log(err))
 
+const Restaurant=require('./models/restaurantmodel')
+
+const foods = [];
+fs.createReadStream('indian_food.csv')
+  .pipe(csvParser())
+  .on('data', (row) => {
+    foods.push(row);
+  })
+  .on('end', () => {
+    console.log('CSV file successfully processed.');
+  });
+app.get('/dishes',(req,res)=>{
+    res.json(foods)
+})
+
+
+
 app.get('/',(req,res)=>{
-    res.send('<h1>Helloo</h1>')
+    res.send('server is working #HOME')
+})
+
+app.post('/restaurant/register',(req,res)=>{
+
+    const {hotelName,password,location,nofTables,listOfDishes} = req.body;
+
+    bcrypt.genSalt(10).then(salt=>bcrypt.hash(password,salt)).then(hashedpwd=>{
+        const restaurant = new Restaurant({hotelName,password:hashedpwd,location,nofTables,listOfDishes})
+        restaurant.save()
+        .then(rslt=>res.send(rslt))
+        .catch(err=>console.log(err))
+    }).catch(err=>{console.log(err)})
+
 })
--- backend/models/restaurantmodel.js
+++ backend/models/restaurantmodel.js
@@ -2,12 +2,18 @@ const mongoose=require('mongoose')
 
 const restaurant_schema=new mongoose.Schema({
     hotelName:String,
-    hotelId:String,
+    password:String,
+    location:String,
     nofTables:Number,
-    nofDishes:Number,
-    listOfDishes:[],
+    listOfDishes:[
+        {
+            dishName:String,
+            price:Number,
+            inStock:Boolean
+        }
+    ],
 })
 
-const Restaurant=mongoose.model('Restaurant',restaurant_schema)
+const Restaurant=mongoose.model('restaurant',restaurant_schema)
 
 module.exports=Restaurant;
